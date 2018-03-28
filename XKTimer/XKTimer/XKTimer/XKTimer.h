4d06d703d55d76e315ecf042cbf9dacf979798ce
浪漫恋星空 <727378500@qq.com>
2018-03-28 18:11:21 +0800

    修改部分细节

--- XKTimer/XKTimer/XKTimer/XKTimer.h
+++ XKTimer/XKTimer/XKTimer/XKTimer.h
@@ -15,14 +15,14 @@
  
  @param seconds 定时器的时间间隔
  @param repeats 定时器是否需要重复
- @param queue 执行线程
+ @param queue   执行线程
  @param handler 间隔执行的操作
  @return 定时器
  */
 + (XKTimer *)xk_timerWIthTimeInterval:(NSTimeInterval)seconds
-                                repeats:(BOOL)repeats
-                                  queue:(dispatch_queue_t)queue
-                                handler:(dispatch_block_t)handler;
+                              repeats:(BOOL)repeats
+                                queue:(dispatch_queue_t)queue
+                              handler:(dispatch_block_t)handler;
 
 /**
  初始化定时器(在主线程)
@@ -33,8 +33,8 @@
  @return 定时器
  */
 + (XKTimer *)xk_timerWIthTimeInterval:(NSTimeInterval)seconds
-                                repeats:(BOOL)repeats
-                                handler:(dispatch_block_t)handler;
+                              repeats:(BOOL)repeats
+                              handler:(dispatch_block_t)handler;
 
 /**
  开始定时器
